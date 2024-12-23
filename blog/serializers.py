081838e9f851293e2071ed6f0496051f978c0b8e
olatunji-weber <olatunji.weber@gmail.com>
2024-12-24 00:59:23 +0200

    Made rectifications to Serializers.py...

--- blog/serializers.py
+++ blog/serializers.py
@@ -4,7 +4,7 @@ from .models import Post, Comment
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ' all '
+        fields = '__all__'
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
