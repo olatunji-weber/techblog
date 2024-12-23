76d17575af2f01db81926189ca7b2b8ab3f2e295
olatunji-weber <olatunji.weber@gmail.com>
2024-12-23 17:44:21 +0200

    Create and register Models, Create views, and setup urls...

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.post}'
